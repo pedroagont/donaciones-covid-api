a80f1ed62b6f2cb68406ad625025fc53c9229f5f
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-04-15 21:29:31 -0600

    Agregue endpoint para documentacion de la API

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 4080;
 require('./database')
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
 // Endpoints
 app.get('/', (req, res) => res.json('¡Bienvenido!'));
+
+// viewed at http://localhost:4080
+app.get('/docs', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 
 // Encender el servidor
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
