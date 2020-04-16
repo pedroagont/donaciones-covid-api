e5bda29a99f6171d78753fe16f8f4ced38c771fa
Aabraham-Farias <abbraham_13905@hotmail.com>
2020-04-16 13:47:20 -0600

    Se agrego los endpoints de la api Articulos

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 4080;
-require('./database')
+require('./database');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-// Endpoints
-app.get('/', (req, res) => res.json('¡Bienvenido!'));
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la plataforma donaciones');
+});
 
 // viewed at http://localhost:4080
-app.get('/doc', function(req, res) {
+app.get('/docs', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use('/api/v1', require('./router'));
 
-// Encender el servidor
-app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- router/index.js
+++ router/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
+const ArticuloRoutes = require('./ArticulosRoutes');
+
+router.use(ArticuloRoutes);
+
 module.exports = router;
